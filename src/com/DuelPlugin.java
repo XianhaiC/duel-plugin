b478fd2a495acf6bb2e8d53b0e2799f6d546735f
Xianhai Cao <xianhaic@gmail.com>
2020-12-22 22:43:52 -0800

    Rename some stuff

--- src/com/DuelPlugin.java
+++ src/com/DuelPlugin.java
@@ -1,13 +1,20 @@
 package com;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import com.pablo67340.SQLiteLib.Main.SQLiteLib;
+
+import com.commands.LeaderboardCommand;
 
 public class DuelPlugin extends JavaPlugin {
+  public SQLiteLib sqlLib;
 
   @Override
   public void onEnable() {
+    sqlLib = SQLiteLib.hookSQLiteLib();
+    sqlLib.initializeDatabase(pluginInstance, "duel", "CREATE TABLE IF NOT EXISTS table_name");
+
     // new ArrowHandler(this);
     // new DamageHandler(this);
-    // this.getCommand("rjboots").setExecutor(new BootsCommand(this));
+    //this.getCommand("duelboards").setExecutor(new LeaderboardCommand(this));
   }
 }
