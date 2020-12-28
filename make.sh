fdb67f785f0dcab4efb3db1cb5f9c6e0bd74a8b0
BuildTools <unconfigured@null.spigotmc.org>
2020-12-27 18:55:51 -0800

    move these files to second branch

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-
+mkdir -p builds
 # compile the main class with the spigot jar
-javac -cp ../../libraries/spigot/spigot-1.16.4.jar:/usr/share/java/sqlite-jdbc/sqlite-jdbc-3.27.2.1.jar src/com/eventhandlers/*.java src/com/commands/*.java src/com/*.java -d classes
+javac -cp ../libraries/spigot-1.16.4.jar:/usr/share/java/sqlite-jdbc/sqlite-jdbc-3.27.2.1.jar src/com/eventhandlers/*.java src/com/commands/*.java src/com/*.java -d classes
 # bundle together into an archive
 jar cvf builds/DuelPlugin.jar -C classes .
